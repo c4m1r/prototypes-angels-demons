54176c63e273137e3b0d329ad9f59ba0817498ac
c4m1r <c4m1rr@gmail.com>
2026-05-21 12:35:03 +0500

    fix(deploy): set vite base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/prototypes-angels-demons/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
